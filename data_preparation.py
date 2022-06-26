9fba3a44be1a6a972b78c6c9a5c358f4a3fca841
Wenyuan Wu <wenyuan.wu@uzh.ch>
2022-06-26 20:12:22 +0200

    initial commit

---
+++ data_preparation.py
@@ -0,0 +1,9 @@
+import os
+from tqdm import tqdm
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    # datefmt='%d-%b-%y %H:%M:%S'
+                    )
+
